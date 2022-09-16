580cfec6068aef104a6838e96b1fc67c3e978589
Fernada1305 <101894024+Fernada1305@users.noreply.github.com>
2022-09-16 17:12:49 -0500

    Update authors.sql

--- authors.sql
+++ authors.sql
@@ -1,31 +1,4 @@
--- phpMyAdmin SQL Dump
--- version 5.1.0
--- https://www.phpmyadmin.net/
---
--- Servidor: 127.0.0.1
--- Tiempo de generación: 16-09-2022 a las 23:20:36
--- Versión del servidor: 10.4.19-MariaDB
--- Versión de PHP: 8.0.6
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de datos: `PLATAFORM_OPERATION`
---
-
--- --------------------------------------------------------
-
---
--- Estructura de tabla para la tabla `authors`
---
 
 CREATE TABLE `authors` (
   `author_id` int(10) UNSIGNED NOT NULL,
@@ -33,9 +6,6 @@ CREATE TABLE `authors` (
   `nationality` varchar(3) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Volcado de datos para la tabla `authors`
---
 
 INSERT INTO `authors` (`author_id`, `name`, `nationality`) VALUES
 (1, 'Juan Rulfo', 'MEX'),
@@ -46,27 +16,15 @@ INSERT INTO `authors` (`author_id`, `name`, `nationality`) VALUES
 (6, 'Octavio Paz', 'MEX'),
 (7, 'Juan Carlos Onetti', 'URU');
 
---
--- Índices para tablas volcadas
---
 
---
--- Indices de la tabla `authors`
---
+
 ALTER TABLE `authors`
   ADD PRIMARY KEY (`author_id`);
 
---
--- AUTO_INCREMENT de las tablas volcadas
---
 
---
--- AUTO_INCREMENT de la tabla `authors`
---
+
 ALTER TABLE `authors`
   MODIFY `author_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
-COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
